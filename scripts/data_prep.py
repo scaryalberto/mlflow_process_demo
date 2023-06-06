03c5217e7d29d251597a5e11993146fcc35c8f1b
Scaringi <alsg@gft.com>
2023-06-06 17:37:27 +0200

    inserita raise e docstring

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -9,21 +9,31 @@ import click
 @click.option("--param_1", type=str)
 @click.option("--param_2", type=str)
 def data_prep(param_1, param_2):
+   """
+   Funzione che rielabora i dataset in formato csv passati.
+
+   Args:
+       param_1 (str): percorso del dataset
+       param_2 (str): percorso del dataset
+
+   """
 
    print("eeeeeeeeeeeeeeeeeee")
    print(param_1)
 
+
+   #TODO: devo scaricare il dataset da un link?
    url = "http://www.example.com/file.csv"
    #df = pd.read_csv(url)
    #print(df.head())
 
-   #TODO: devo scaricare il dataset da un link?
-   import pdb
-   #pdb.set_trace()
-   df = pd.read_csv(param_1)
-   #"C:\\Users\\alsg\\PycharmProjects\\mlflow_mio_per_github\\dataset\\markers_quartieri.csv"
+   if param_1.endswith('.csv'):
+      df = pd.read_csv(param_1)
+      #"C:\\Users\\alsg\\PycharmProjects\\mlflow_mio_per_github\\dataset\\markers_quartieri.csv"
+      print("letto csv")
 
-   print("letto csv")
+   else:
+      raise "Non è un csv"
 
 
 if __name__ == '__main__':
